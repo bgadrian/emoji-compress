f3fd533653f0d88b89b023d0a08327f410056c36
davidescus <rahthman_s@yahoo.com>
2017-11-15 16:24:24 +0200

    Add testing table and example.

--- lz78/decompress_test.go
+++ lz78/decompress_test.go
@@ -6,48 +6,38 @@ import (
 	"testing"
 )
 
-type casesInOut []inOut
-
-type inOut struct {
-	in  string
-	out string
+type table struct {
+	name string
+	in   string
+	out  string
 }
 
-func ExampleDecompressString() {
-	in := "|0000a||0000b||0001b||0000c||0002a|"
-	out, err := Decompress(in)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Printf("Decompressed string: %s", out)
-	// Output: Decompressed string: ababcba
-}
+func TestDecompress(t *testing.T) {
 
-func TestDecompressInOut(t *testing.T) {
-
-	c := casesInOut{
-		{
-			in:  "|0000a||0000b||0001b||0000c||0002a|",
-			out: "ababcba",
-		},
-		{
-			in:  "|0000a||0000b||0001b||0003c|",
-			out: "abababc",
-		},
-		{
-			in:  "|0000|||0000b||0001b||0003c|",
-			out: "|b|b|bc",
-		},
+	tt := []table{
+		{"Test1", "|0000a||0000b||0001b||0000c||0002a|", "ababcba"},
+		{"Test2", "|0000a||0000b||0001b||0003c|", "abababc"},
+		{"Test3", "|0000|||0000b||0001b||0003c|", "|b|b|bc"},
 	}
 
-	for _, e := range c {
+	for _, e := range tt {
 		r, err := Decompress(e.in)
 		if err != nil {
 			t.Error(err)
 		}
 
 		if r != e.out {
-			t.Errorf("Expected %s, got %s", e.out, r)
+			t.Errorf("In %s expected %s, got %s", e.name, e.out, r)
 		}
 	}
+}
+
+func ExampleDecompress() {
+	in := "|0000P||0000l||0000a||0000y||0000 ||0000w||0000i||0000t||0000h||0005e||0000m||0000o||0000j||0007s||0000!|"
+	out, err := Decompress(in)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("%s", out)
+	// Output: Play with emojis!
 }
