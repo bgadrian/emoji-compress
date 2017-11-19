acf46ac3afc799e7fe54ed2383a9ea8dc7420f98
B.G.Adrian <btools@gmail.com>
2017-11-19 14:22:41 +0200

    Demo website comments, css and made dictionary to work.

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,13 +1,13 @@
 
 var API = {
-    base: "http://appengine.emoji-compress.com/",
-    // base: "http://127.0.0.1:8080/",
-    _run: function (payload, handler, callback) {
+    // base: "https://appengine.emoji-compress.com/",
+    base: "http://127.0.0.1:8080/",
+    call: function (payload, handler, callback) {
         var targetUri = new URL(this.base + handler);
+        //https://developer.mozilla.org/en-US/docs/Web/API/Request/Request
         var webRequest = new Request(targetUri,
             { method: "POST", mode: "cors", body: JSON.stringify(payload) });
 
-        //https://developer.mozilla.org/en-US/docs/Web/API/Request/Request
         fetch(webRequest)
             .then(function (response) {
                 if (response.status == 200) return response.json();
@@ -24,43 +24,52 @@ var API = {
                 callback(null, reason)
             })
     },
-    bytesmap: {
-        encode: function (original, callback) {
-            var payload = { text: original }
-            var handler = "bytesmap/encode"
-            API._run(payload, handler, callback)
-        },
-        decode: function (original, callback) {
-            var payload = { text: original }
-            var handler = "bytesmap/decode"
-            API._run(payload, handler, callback)
-        }
-    }
 }
 
 // API.bytesmap.encode("a", (result, error) => { console.info(result); console.error(error) })
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    var watch = function (handler, func) {
-        var idprefix = handler + "_" + func + "_"
+    var watchSimple = function (handler, method) {
+        var idprefix = handler + "_" + method + "_"
         var button = document.getElementById(idprefix + "btn")
         var textIn = document.getElementById(idprefix + "in")
         var textOut = document.getElementById(idprefix + "out")
+        var url = handler + "/" + method
+
+        var dictIn = document.getElementById(idprefix + "dictionary_in")
+        var dictOut = document.getElementById(idprefix + "dictionary_out")
 
         button.addEventListener("click", function () {
-            API[handler][func](textIn.value, function (result, error) {
+            API.call({
+                text: textIn.value,
+                dict: (dictIn ? JSON.parse(dictIn.value) : null)
+            }, url, function (apiResponse, error) {
                 if (error) {
                     textOut.value = "Error: " + error
                     return
                 }
-                textOut.value = result
+                // console.info(apiResponse)
+
+                if (dictOut) {
+                    //we are expecting a dictionary out so ...
+                    textOut.value = apiResponse.archive
+                    dictOut.value = JSON.stringify(apiResponse.dict)
+                } else {
+                    textOut.value = apiResponse
+                }
             })
         })
     }
 
-    watch("bytesmap", "encode")
-    watch("bytesmap", "decode")
+    watchSimple("bytesmap", "encode")
+    watchSimple("bytesmap", "decode")
+
+    watchSimple("lz78", "encode")
+    watchSimple("lz78", "decode")
+
+    watchSimple("dictionary", "encode")
+    watchSimple("dictionary", "decode")
 
 }, false);
 
